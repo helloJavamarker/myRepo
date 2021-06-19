b5494750ab0e8d382af73415c10d72935ac44e74
mark <zz18595723428@163.com>
2021-06-19 16:44:56 +0800

    arthas

---
+++ src/main/java/com/test/mark/zhang/test/other/arthas/ArthasTest.java
@@ -0,0 +1,14 @@
+package com.test.mark.zhang.test.other.arthas;
+
+/**
+ * @Classname ArthasTest
+ * @Description TODO
+ * @Date 2021/6/18 7:46 下午
+ * @Created by mark
+ */
+public class ArthasTest {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/test/mark/zhang/test/other/arthas/MathGame.java
@@ -0,0 +1,62 @@
+package com.test.mark.zhang.test.other.arthas;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class MathGame {
+    private static Random random = new Random();
+    private int illegalArgumentCount = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        MathGame game = new MathGame();
+        do {
+            game.run();
+            TimeUnit.SECONDS.sleep(1L);
+        } while (true);
+    }
+
+    public void run() throws InterruptedException {
+        try {
+            int number = random.nextInt();
+            List<Integer> primeFactors = this.primeFactors(number);
+            MathGame.print(number, primeFactors);
+        } catch (Exception e) {
+            System.out.println(String.format("illegalArgumentCount:%3d, ", this.illegalArgumentCount) + e.getMessage());
+        }
+    }
+
+    public static void print(int number, List<Integer> primeFactors) {
+        StringBuffer sb = new StringBuffer("" + number + "=");
+        Iterator<Integer> iterator = primeFactors.iterator();
+        while (iterator.hasNext()) {
+            int factor = iterator.next();
+            sb.append(factor).append('*');
+        }
+        if (sb.charAt(sb.length() - 1) == '*') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        System.out.println(sb);
+    }
+
+    public List<Integer> primeFactors(int number) {
+        if (number < 2) {
+            ++this.illegalArgumentCount;
+            throw new IllegalArgumentException("number is: " + number + ", need >= 2");
+        }
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int i = 2;
+        while (i <= number) {
+            if (number % i == 0) {
+                result.add(i);
+                number /= i;
+                i = 2;
+                continue;
+            }
+            ++i;
+        }
+        return result;
+    }
+}
