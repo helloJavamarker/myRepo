acd7691ad0e67213abdd18383f4e3ec390730cea
mark <zz18595723428@163.com>
2021-09-18 16:29:29 +0800

    git mod2

--- security/src/main/java/com/example/security/git/TestA.java
+++ security/src/main/java/com/example/security/git/TestA.java
@@ -10,5 +10,7 @@ public class TestA {
 
     public static void main(String[] args) {
         System.out.println("testA:");
+
+        System.out.println("======");
     }
 }
--- security/src/main/java/com/example/security/git/TestB.java
+++ security/src/main/java/com/example/security/git/TestB.java
@@ -11,5 +11,6 @@ public class TestB {
 
     public static void main(String[] args) {
         System.out.println("main:" + "testB");
+        System.out.println("............");
     }
 }
